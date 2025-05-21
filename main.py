48463bae9c7af26d76ee725550421fbaae017a65
emmanuella-appiah <emmanuella@reallygreattech.com>
2025-05-21 11:00:26 +0000

    Updates to code: Still in dev mode

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from scripts.ingest import  parse_bible_file
+from scripts.pipeline import build_rag_chain
+
+# Load environment variables from .env
+load_dotenv()
+
+# Load LLM
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', temperature=0)
+
+# Set paths
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, 'db', 'chroma_bible.db')
+file_path = os.path.join(current_dir, 'data', 'kjv.txt')
+
+# initialize embedding
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+query = "What does God say about anxiety? How does this relate to what we discussed earlier about trust?"
+
+parse_bible_file(file_path, persistent_directory,embedding_model)
+rag_chain = build_rag_chain(
+    llm=llm,
+    persistent_directory=persistent_directory,
+    embeddings=embedding_model
+)
+
+rag_chain.invoke({
+    "input": query,
+    "chat_history": [],
+})
+
+
---
+++ scripts/ingest.py
@@ -0,0 +1,37 @@
+import os
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+def parse_bible_file(file_path: str, persistent_directory: str, embeddings):
+    if not os.path.exists(persistent_directory):
+        print("Persistent directory does not exist. Initializing vector store...")
+
+        # Ensure the text file exists
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(
+                f"The file {file_path} does not exist. Please check the path."
+            )
+
+        # Read the text content from the file
+        loader = TextLoader(file_path)
+        documents = loader.load()
+
+        # Split the document into chunks
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
+
+        # Create embeddings
+        embeddings = embeddings
+
+        # Create the vector store and persist it automatically
+        db = Chroma.from_documents(
+            documents=docs,
+            embedding=embeddings,
+            persist_directory=persistent_directory
+        )
+
+    else:
+        print("Vector store already exists. No need to initialize.")
